76f0c66cbac632154a7b17467330cd46de0d1d5f
Robby Kim <robert.st.kim@gmail.com>
2016-09-29 16:29:47 -0400

    still setting up auth0

--- src/js/components/popup-container.jsx
+++ src/js/components/popup-container.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import jwtDecode from 'jwt-decode';
 import Popup from './popup';
 
 export default class PopupContainer extends React.Component {
   constructor() {
     super();
     this.setPage = this.setPage.bind(this);
+    this.getProfile = this.getProfile.bind(this);
     this.state = {
+      profile: {},
+      error: '',
+      token: null,
       isAuthenticated: false,
       page: {},
     };
@@ -19,6 +24,41 @@ export default class PopupContainer extends React.Component {
     }
   }
 
+  getProfile(lock, authResult) {
+    lock.getProfile(authResult.idToken, (err, profile) => {
+      if (err) {
+        this.setState({
+          error: err,
+        });
+      }
+
+      this.setState({
+        profile,
+        token: authResult.idToken,
+      });
+    });
+  }
+
+  componentWillMount() {
+    const jwt = this.state.token;
+    console.log(jwt);
+    // if (jwt) {
+    //   const jwtExp = jwtDecode(jwt).exp;
+    //   const expiryDate = new Date(0);
+    //   expiryDate.setUTCSeconds(jwtExp);
+    //
+    //   if (new Date() < expiryDate) {
+    //     return this.setState({
+    //       isAuthenticated: true,
+    //     });
+    //   }
+    // }
+    //
+    return this.setState({
+      isAuthenticated: false,
+    });
+  }
+
   render() {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
       this.setPage(tabs[0]);
@@ -28,6 +68,7 @@ export default class PopupContainer extends React.Component {
       <Popup
         isAuthenticated={this.state.isAuthenticated}
         page={this.state.page}
+        getProfile={this.getProfile}
       />
     );
   }
--- src/js/components/popup.jsx
+++ src/js/components/popup.jsx
@@ -1,55 +1,71 @@
 import React from 'react';
+import Auth0Lock from 'auth0-lock';
 
 export default class Popup extends React.Component {
-  clicked(tab) {
-
-  }
-
   render() {
-    const { isAuthenticated, page } = this.props;
-    // async actions making `page` undefined before it gets used by tab
+    const { isAuthenticated, page, getProfile } = this.props;
+    const lock = new Auth0Lock('6ElpyE9EazmBox2b9PAWytCnFJQTxBCa', 'ericsnell.auth0.com', {
+      auth: {
+        responseType: 'token',
+        params: {
+          scope: 'openid name identities picture',
+        },
+      },
+    });
+
+    lock.on('authenticated', (authResult) => {
+      getProfile(lock, authResult);
+    });
+
+    if (isAuthenticated) {
+      return (
+        <div>
+          <h1>Book Kit</h1>
+          <form>
+            <label htmlFor="form-title">Title *</label>
+            <input
+              type="text"
+              ref={(title) => { this.title = title; }}
+              id="form-title"
+              // defaultValue={page.title}
+              required
+            />
+            <label htmlFor="form-url">URL *</label>
+            <input
+              type="text"
+              ref={(url) => { this.url = url; }}
+              id="form-url"
+              // defaultValue={page.url}
+              required
+            />
+            <label htmlFor="form-description">Description</label>
+            <textarea
+              ref={(description) => { this.description = description; }}
+              id="form-description"
+            />
+            <label htmlFor="form-screenshot">Screenshot</label>
+            <input
+              type="text"
+              ref={(screenshot) => { this.screenshot = screenshot; }}
+              id="form-screenshot"
+            />
+            {/* <label htmlFor="form-folder">Folder *</label>
+            <select
+              ref={folder => { this.folder = folder; }}
+              id="form-folder"
+              className="folder-dropdown"
+              required
+            >{folderArr}
+            </select> */}
+            <button>Submit</button>
+          </form>
+        </div>
+      );
+    }
 
     return (
       <div>
-        <h1>Book Kit</h1>
-        <form>
-          <label htmlFor="form-title">Title *</label>
-          <input
-            type="text"
-            ref={(title) => { this.title = title; }}
-            id="form-title"
-            // defaultValue={page.title}
-            required
-          />
-          <label htmlFor="form-url">URL *</label>
-          <input
-            type="text"
-            ref={(url) => { this.url = url; }}
-            id="form-url"
-            // defaultValue={page.url}
-            required
-          />
-          <label htmlFor="form-description">Description</label>
-          <textarea
-            ref={(description) => { this.description = description; }}
-            id="form-description"
-          />
-          <label htmlFor="form-screenshot">Screenshot</label>
-          <input
-            type="text"
-            ref={(screenshot) => { this.screenshot = screenshot; }}
-            id="form-screenshot"
-          />
-          {/* <label htmlFor="form-folder">Folder *</label>
-          <select
-            ref={folder => { this.folder = folder; }}
-            id="form-folder"
-            className="folder-dropdown"
-            required
-          >{folderArr}
-          </select> */}
-          <button>Submit</button>
-        </form>
+        <button onClick={() => { lock.show(); }}>Login</button>
       </div>
     );
   }
